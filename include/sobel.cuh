aa9bfadc3e7911e40d35bf4429059cbc26125d49
Can <selc911@gmaile.com>
2026-09-17 21:07:57 +0800

    Résolution conflit compilateur

--- include/sobel.cuh
+++ include/sobel.cuh
@@ -6,10 +6,13 @@
 // Déclaration de la version CPU
 void sobelCPU(const std::vector<unsigned char>& in, std::vector<unsigned char>& out, int width, int height);
 
+
+#ifdef __CUDACC__
 // Déclaration de la version GPU Naïve (Mémoire Globale)
 __global__ void sobelGPUNaive(const unsigned char* __restrict__ d_in, unsigned char* __restrict__ d_out, int width, int height);
 
 // Déclaration de la version GPU Optimisée (Mémoire Partagée + Coalescence)
 __global__ void sobelFilter(const unsigned char* __restrict__ d_in, unsigned char* __restrict__ d_out, int width, int height);
+#endif
 
 #endif
