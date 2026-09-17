dceb050ad86b7b03220fe75a3f1e4c3d9bf64eb1
Can <selc911@gmaile.com>
2026-09-17 20:49:07 +0800

    Correction fichier

--- src/sobel_naive.cu
+++ src/sobel_naive.cu
@@ -1,6 +1,6 @@
 
 
-__global__ void sobelGPU(const int* __restrict__ d_in, int* __restrict__ d_out, int width, int height) {
+__global__ void sobelGPUNaive(const int* __restrict__ d_in, int* __restrict__ d_out, int width, int height) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
